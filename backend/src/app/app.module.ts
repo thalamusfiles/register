f727e35f7e6c73ea821fdfb2dddcaaad3d80ed39
piemontez <rafaelpiemontez@gmail.com>
2023-05-05 15:30:47 -0300

    revisão docker

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AppApiModule } from './api/appapi.module';
 import ModelModule from './model.module';
+import { StaticFileModule } from './staticfiles.module';
 
 @Module({
   imports: [
     //
     ModelModule,
+    AppApiModule,
+    StaticFileModule,
   ],
   controllers: [],
   providers: [],
