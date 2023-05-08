0f57fa3fe584c5d783c6bd4bdad0a88d7ffdcf2c
piemontez <rafaelpiemontez@gmail.com>
2023-05-08 06:39:47 -0300

    builds

---
+++ bin/local-build-docs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Entra na pasta bin
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+# Troca para a pasta docs
+cd ../docs/
+
+# Inicia a aplicação
+yarn
+
+# Inicia a aplicação
+yarn build
---
+++ bin/local-build-frontend.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Entra na pasta bin
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+echo "$parent_path"
+
+# Troca para a pasta do frontend
+cd ../frontend_web
+
+# Inicia a aplicação
+yarn
+
+# Inicia a aplicação
+yarn build
