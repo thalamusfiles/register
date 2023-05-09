68890cb210244508c604492fa15594626d2caafc
piemontez <rafaelpiemontez@gmail.com>
2023-05-09 09:37:29 -0300

    migrations

--- backend/src/migrations/Migration20230509004000_establishment.ts
+++ backend/src/migrations/Migration20230509004000_establishment.ts
@@ -3,41 +3,58 @@ import { Migration } from '@mikro-orm/migrations';
 export class Migration20230509115642 extends Migration {
   async up(): Promise<void> {
     this.addSql(
-      'create table "establishment" ("uuid" uuid not null default uuid_generate_v4(), "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "resource_country_acronym" varchar(4) not null, "resource_uuid" uuid not null, "person_uuid" uuid not null, "country_uuid" uuid not null, "city_uuid" uuid not null, "data" jsonb not null, constraint "establishment_pkey" primary key ("uuid"));',
+      `create table "establishment" (
+        "uuid" uuid not null default uuid_generate_v4(), 
+        "created_at" timestamptz(0) not null, 
+        "updated_at" timestamptz(0) not null, 
+        "resource_country_acronym" varchar(4) not null, 
+        "resource_uuid" uuid not null, 
+        "person_uuid" uuid not null, 
+        "country_uuid" uuid not null, 
+        "city_uuid" uuid not null, 
+        "data" jsonb not null, 
+        
+        constraint "establishment_pkey" primary key ("uuid")
+      );`,
     );
 
     this.addSql(
-      'alter table "establishment" add constraint "establishment_resource_country_acronym_foreign" foreign key ("resource_country_acronym") references "resource_country" ("acronym") on update cascade;',
+      `alter table "establishment" add constraint "establishment_resource_country_acronym_foreign"
+       foreign key ("resource_country_acronym") references "resource_country" ("acronym") on update cascade;`,
     );
 
     this.addSql(
-      'alter table "establishment" add constraint "establishment_resource_uuid_foreign" foreign key ("resource_uuid") references "resource" ("uuid") on update cascade;',
+      `alter table "establishment" add constraint "establishment_resource_uuid_foreign"
+       foreign key ("resource_uuid") references "resource" ("uuid") on update cascade;`,
     );
 
     this.addSql(
-      'alter table "establishment" add constraint "establishment_person_uuid_foreign" foreign key ("person_uuid") references "person" ("uuid") on update cascade;',
+      `alter table "establishment" add constraint "establishment_person_uuid_foreign"
+       foreign key ("person_uuid") references "person" ("uuid") on update cascade;`,
     );
 
     this.addSql(
-      'alter table "establishment" add constraint "establishment_country_uuid_foreign" foreign key ("country_uuid") references "address"."country" ("uuid") on update cascade;',
+      `alter table "establishment" add constraint "establishment_country_uuid_foreign"
+       foreign key ("country_uuid") references "address"."country" ("uuid") on update cascade;`,
     );
 
     this.addSql(
-      'alter table "establishment" add constraint "establishment_city_uuid_foreign" foreign key ("city_uuid") references "address"."city" ("uuid") on update cascade;',
+      `alter table "establishment" add constraint "establishment_city_uuid_foreign"
+       foreign key ("city_uuid") references "address"."city" ("uuid") on update cascade;`,
     );
   }
 
   async down(): Promise<void> {
-    this.addSql('alter table "establishment" drop constraint "establishment_resource_uuid_foreign";');
+    this.addSql(`alter table "establishment" drop constraint "establishment_resource_uuid_foreign";`);
 
-    this.addSql('alter table "establishment" drop constraint "establishment_resource_country_acronym_foreign";');
+    this.addSql(`alter table "establishment" drop constraint "establishment_resource_country_acronym_foreign";`);
 
-    this.addSql('alter table "establishment" drop constraint "establishment_person_uuid_foreign";');
+    this.addSql(`alter table "establishment" drop constraint "establishment_person_uuid_foreign";`);
 
-    this.addSql('alter table "establishment" drop constraint "establishment_country_uuid_foreign";');
+    this.addSql(`alter table "establishment" drop constraint "establishment_country_uuid_foreign";`);
 
-    this.addSql('alter table "establishment" drop constraint "establishment_city_uuid_foreign";');
+    this.addSql(`alter table "establishment" drop constraint "establishment_city_uuid_foreign";`);
 
-    this.addSql('drop table if exists "establishment" cascade;');
+    this.addSql(`drop table if exists "establishment" cascade;`);
   }
 }
