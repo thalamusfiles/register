8f260fd8b744810b34d8681ce84de87e2c7e5e80
piemontez <rafaelpiemontez@gmail.com>
2023-05-31 08:03:04 -0300

    table type key value

---
+++ backend/src/migrations/Migration20230509005000_type_key_value.ts
@@ -0,0 +1,37 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20230531094013 extends Migration {
+  async up(): Promise<void> {
+    this.addSql(
+      `create table "type_key_value" (
+        "uuid" uuid not null default uuid_generate_v4(), 
+        "created_at" timestamptz(0) not null, 
+        "updated_at" timestamptz(0) not null, 
+        "resource_country_acronym" varchar(4) not null, 
+        "resource_uuid" uuid not null, 
+        
+        "type" varchar(255) not null, 
+        "key" varchar(255) not null, 
+        "value" jsonb not null, 
+        
+        "deleted_at" timestamptz(0) null, 
+        
+        constraint "type_key_value_pkey" primary key ("uuid"));`,
+    );
+    this.addSql(
+      `alter table "type_key_value" add constraint "type_key_value_resource_country_acronym_resource_u_ab2ab_unique"
+       unique ("resource_country_acronym", "resource_uuid", "type", "key");`,
+    );
+
+    this.addSql(
+      'alter table "type_key_value" add constraint "type_key_value_resource_country_acronym_foreign" foreign key ("resource_country_acronym") references "resource_country" ("acronym") on update cascade;',
+    );
+    this.addSql(
+      'alter table "type_key_value" add constraint "type_key_value_resource_uuid_foreign" foreign key ("resource_uuid") references "resource" ("uuid") on update cascade;',
+    );
+  }
+
+  async down(): Promise<void> {
+    this.addSql('drop table if exists "type_key_value" cascade;');
+  }
+}
---
+++ backend/src/model/TypeKeyValue.ts
@@ -0,0 +1,26 @@
+import { Entity, ManyToOne, Property, Unique } from '@mikro-orm/core';
+import { RegisterBaseEntity } from './Base/RegisterBaseEntity';
+import { Resource } from './Resource';
+import { ResourceCountry } from './ResourceCountry';
+
+@Entity({ schema: 'public', readonly: true })
+@Unique({ properties: ['resourceCountry', 'resource', 'type', 'key'] })
+export class TypeKeyValue extends RegisterBaseEntity {
+  @ManyToOne(() => ResourceCountry, { nullable: false })
+  resourceCountry?: ResourceCountry;
+
+  @ManyToOne(() => Resource, { nullable: false })
+  resource!: Resource;
+
+  @Property({ nullable: false })
+  type!: string;
+
+  @Property({ nullable: false })
+  key!: string;
+
+  @Property({ type: 'json', nullable: false })
+  value!: string;
+
+  @Property({ nullable: true })
+  deletedAt?: Date;
+}
