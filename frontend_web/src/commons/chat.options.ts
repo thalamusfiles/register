5e43215fb06d5317f2ae5dd2f456e36472817024
piemontez <rafaelpiemontez@gmail.com>
2023-07-03 14:42:22 -0300

    fix bar build

--- frontend_web/src/commons/chat.options.ts
+++ frontend_web/src/commons/chat.options.ts
@@ -1,6 +1,6 @@
 import { ChartOptions } from 'chart.js';
 
-export const ChartBarOptions: ChartOptions = {
+export const ChartBarOptions: ChartOptions<"bar"> = {
   scales: {
     x: {
       grid: {
