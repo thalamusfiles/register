c283e1923a6a6da85b63f586b5132b1959f31fb7
piemontez <rafaelpiemontez@gmail.com>
2023-08-09 12:14:42 -0300

    inicio do sistema de documentação

--- bin/docker-run-docs.sh
+++ bin/docker-run-docs.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 
-FILE=config.env
+#Configurações
+GATSBY_TELEMETRY_DISABLED=1
+PORT=4002
 
 # Entra na pasta bin
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
-
-# Carrega configurações
-if [ -f "$FILE" ]; then
-    export $(grep -v '^#' $FILE | xargs -d '\n')
-else
-  echo "Arquivo $FILE inexistente."
-  exit 1
-fi
-
 
 # Troca para a pasta docs
 cd ../docs/
 
 # Inicia a aplicação
 echo "Builda a imagem"
-docker build -t iam_docs_prod .
+docker build -t register_docs_prod .
 
 echo "Para e remove o container atual se existir"
-docker stop iam_docs_prod || true
-docker container rm iam_docs_prod || true
+docker stop register_docs_prod || true
+docker container rm register_docs_prod || true
 
 echo "Roda o container"
     docker run \
         -p $PORT:9000 \
         -v public:/var/www/docs/public:ro \
-        --name iam_docs_prod \
-        -d iam_docs_prod
+        --name register_docs_prod \
+        -d register_docs_prod
