161badcd2796603a0bd9227f5e559378958783ff
piemontez <rafaelpiemontez@gmail.com>
2023-08-10 10:11:00 -0300

    revisão dos logs do sistema

---
+++ backend/src/config/billing.products.ts
@@ -0,0 +1,19 @@
+const productsNames = {
+  AddressFindStates: 'register.address.findStates',
+  AddressFindCitiesByState: 'register.address.findCitiesByState',
+  EstabFindByZipcode: 'register.establishment.findByZipcode',
+  EstabFindByZipcodeRandom: 'register.establishment.findByZipcodeRandom',
+  EstabFindByBusinessType: 'register.establishment.findByBusinessType',
+  EstabFindByBusinessTypeRandom: 'register.establishment.findByBusinessTypeRandom',
+  PersonFindLegalByDocument: 'register.person.findByZipcode',
+  PersonFindLegalByRandom: 'register.person.findLegalByRandom',
+  PersonFindNaturalByDocument: 'register.person.findNaturalByDocument',
+  PersonFindNaturalByRandom: 'register.person.findNaturalByRandom',
+  TypeKeyValueFindBRCNAES: 'register.typeKeyValue.findBRCNAES',
+  RelEstabTotalByMonthAndState: 'register.relEstablishment.totalByMonthAndState',
+  RelEstabTotalByMonthAndStateCrosstab: 'register.relEstablishment.totalByMonthAndStateCrosstab',
+  RelEstabTotalByMonthAndNature: 'register.relEstablishment.totalByMonthAndNature',
+  RelEstabTotalByMonthAndMainActivity: 'register.relEstablishment.totalByMonthAndMainActivity',
+};
+
+export default productsNames;
