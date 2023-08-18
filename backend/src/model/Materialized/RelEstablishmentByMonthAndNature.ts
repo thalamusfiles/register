b25f7bb2745e7e135471f2c9b99f9e2435905ff0
piemontez <rafaelpiemontez@gmail.com>
2023-08-18 18:51:58 -0300

    novos indices

--- backend/src/model/Materialized/RelEstablishmentByMonthAndNature.ts
+++ backend/src/model/Materialized/RelEstablishmentByMonthAndNature.ts
@@ -1,9 +1,11 @@
-import { Entity, Property } from '@mikro-orm/core';
+import { Entity, ManyToOne, Property } from '@mikro-orm/core';
+import { TypeKeyValue } from '../TypeKeyValue';
 
 @Entity({
   schema: 'materialized',
   readonly: true,
-  expression: `select begin_date, nature_code, nature, total from "materialized".rel_establishment_by_month_and_nature`,
+  expression: `select begin_date, nature_code, nt."value"->>'description' as nature, total from "materialized".rel_establishment_by_month_and_nature
+    inner join type_key_value nt on nt.hash_id = nature_hash_id`,
 })
 export default class RelEstablishmentByMonthAndNature {
   @Property()
@@ -12,8 +14,8 @@ export default class RelEstablishmentByMonthAndNature {
   @Property()
   natureCode?: string;
 
-  @Property()
-  nature?: string;
+  @ManyToOne(() => TypeKeyValue)
+  nature?: TypeKeyValue;
 
   @Property()
   total?: number;
