cbc1b111d88accb7eeb762da4256b502f4fa46e6
piemontez <rafaelpiemontez@gmail.com>
2023-08-18 17:58:13 -0300

    novos indices

--- backend/src/model/Establishment.ts
+++ backend/src/model/Establishment.ts
@@ -8,6 +8,10 @@ import { ResourceCountry } from './ResourceCountry';
 
 @Entity({ schema: 'public', readonly: true })
 @Index({ properties: ['person'], expression: 'create index establishment_person_hash_id_idx on "establishment" using hash (person_hash_id)' })
+@Index({
+  properties: ['city', 'mainActivity'],
+  expression: 'create index establishment_city_main_activity_idx on "establishment" using hash (city_hash_id, main_activity)',
+})
 export class Establishment extends RegisterBaseEntity {
   @ManyToOne(() => ResourceCountry, { nullable: false })
   resourceCountry?: ResourceCountry;
