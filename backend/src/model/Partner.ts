cece423ac169fa7b15f1283641d4a61001210ddf
piemontez <rafaelpiemontez@gmail.com>
2023-09-01 08:42:04 -0300

    find by socios

--- backend/src/model/Partner.ts
+++ backend/src/model/Partner.ts
@@ -5,10 +5,8 @@ import { Resource } from './Resource';
 import { ResourceCountry } from './ResourceCountry';
 
 @Entity({ schema: 'public', readonly: true })
-@Index({
-  properties: ['establishment'],
-  expression: 'create index partner_establishment_hash_id_idx on "partner" (establishment_hash_id)',
-})
+@Index({ properties: ['establishment'], expression: 'create index partner_establishment_hash_id_idx on "partner" (establishment_hash_id)' })
+@Index({ properties: ['extraKey'], expression: 'create index partner_extra_key_idx on "partner" USING hash (extra_key)' })
 export class Partner extends RegisterBaseEntity {
   @ManyToOne(() => ResourceCountry, { nullable: false })
   resourceCountry?: ResourceCountry;
